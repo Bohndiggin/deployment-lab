922463009844f010896cf218d5f1980d8cad1896
Bohndiggin <bohndiggin@gmail.com>
2022-11-02 15:31:00 -0600

    now it works lol

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 require('dotenv').config()
 const path = require('path');
 
 const app = express()
 
 app.use(express.json())
-app.use(cors)
+// app.use(cors)
 
 const { deliverHome, deliverJS, deliverCSS } = require('./controller')
 
